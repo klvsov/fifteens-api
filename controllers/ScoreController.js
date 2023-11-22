aa6bd370bd72b9a8e2771965ba21e879506a193a
klvsov <klvsov@gmail.com>
2023-11-22 19:38:51 +0200

    set express

--- controllers/ScoreController.js
+++ controllers/ScoreController.js
@@ -1,21 +1,23 @@
 const Score = require('../schemas/ScoreSchema');
 
-exports.getBestScores = async () => {
+exports.getBestScores = async (req, res) => {
   try {
     const scores = await Score.find().sort({ score: 1 }).limit(10);
-    return scores;
+    res.json(scores);
   } catch (error) {
     console.log({ error });
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 };
 
-exports.createScore = async (req) => {
+exports.createScore = async (req, res) => {
   try {
     const { user, score } = req.body;
     const newScore = new Score({ user, score });
-    const savedScore = newScore.save();
-    return savedScore;
+    const savedScore = await newScore.save();
+    res.json(savedScore);
   } catch (error) {
     console.log({ error });
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 };
---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const scoreRouter = require('./routes/ScoreRoutes');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const mongoDBUri = process.env.MONGODB_URI;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect(mongoDBUri);
+const con = mongoose.connection;
+app.use(express.json());
+try {
+  con.on('open', () => {
+    console.log('connected');
+  });
+} catch (error) {
+  console.log('Error: ' + error);
+}
+
+app.use('/', scoreRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
